d7802f2236561943bd238203db405ab98f09bfc5
nick black <nick.black@sprezzatech.com>
2012-11-15 19:14:25 -0500

    Elias Pipping <pipping@exherbo.org>'s patch from the jbig2dec mailing list to return error when tests fail

--- test_jbig2dec.py
+++ test_jbig2dec.py
@@ -52,6 +52,7 @@ class SelfTestSuite:
 	(len(self.fails),len(self.tests)))
     else:
       self.stream.write('PASSED all %d tests\n' % len(self.tests))
+    return len(self.fails) == 0
 
 class KnownFileHash(SelfTest):
   'self test to check for correct decode of known test files'
